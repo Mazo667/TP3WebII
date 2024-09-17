1a227431be0594c18c2bd7ff25dae91c6e423496
Mazo667 <maxifava667@gmail.com>
2024-09-17 10:22:29 -0300

    Implementacion de URLs generadas dinamicamente

--- Blog/post/models.py
+++ Blog/post/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db.models.functions import Now
 from django.conf import settings
+from django.urls import reverse
+from django.utils import timezone
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -14,14 +15,16 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date='publish')
     body = models.TextField()
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name='blog_posts'
     )
-    publish = models.DateTimeField(db_default=Now())
+    publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(
@@ -41,3 +44,12 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.title}: {self.body} [{self.slug}] / ({self.publish}). STATUS: {self.status}'
+    
+    def get_absolute_url(self):
+        return reverse('post:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day,
+            self.slug]
+            )
+    
--- Blog/post/views.py
+++ Blog/post/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from .models import Post
+from django.shortcuts import get_object_or_404
 
 def post_list(request):
     posts = Post.published.all()
     return render(request,'post/list.html',{'posts':posts})
 
-def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("Post no encontrado")
+def post_detail(request, year,month,day,post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day)
     return render(request,'post/detail.html',{'post':post})
